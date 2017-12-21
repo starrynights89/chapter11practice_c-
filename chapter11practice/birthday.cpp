be68204875364c8bff101dffed7d1e0716959f87
Alexander Hartson <alexander-hartson@live.com>
2017-12-21 17:16:53 -0600

    Add files via upload

--- chapter11practice/birthday.cpp
+++ chapter11practice/birthday.cpp
@@ -1,10 +1,37 @@
+//C++ program to convert a decimal to number 
+//to a hex and octal number
+
 #include "std_lib_facilities.h"
+
+//function to convert decimal to octal
+void decToOct(int n)
+{
+	//array to store octal number
+	int octalNum[100];
+
+	// counter for octal number array
+	int i = 0;
+	while (n != 0)
+	{
+		octalNum[i] = n % 8;
+		n += n /8;
+		i++;
+	}
+
+	//printing octal number array in reverse order
+	for (int j = i - 1; j >= 0; j--)
+	{
+		cout << octalNum[j];
+	}
+}
+
+//function to convert decimal to hex
 
 int main()
 {
-    cout << 08231989 << "\t(decimal)\n"
-    	 << 08231989 << "\t(hexadecimal)\n"
-    	 << 08231989 << "\t(octal)\n";
+	int n = 1234;
+
+	decToOct(n);
 
     keep_window_open();
     return 0;
