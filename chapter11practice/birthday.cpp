2894f2f594bbc11e5afbab358c56a89b2dd3286c
Alexander Hartson <alexander-hartson@live.com>
2017-12-21 21:03:46 -0600

    Add files via upload

--- chapter11practice/birthday.cpp
+++ chapter11practice/birthday.cpp
@@ -26,12 +26,34 @@ void decToOct(int n)
 }
 
 //function to convert decimal to hex
+void decToHex(int n)
+{
+	//array to store hexadecimal number
+	int hexadecimalNum[100];
+
+	//array to store hexadecimal number array
+	int i = 0;
+	while (n != 0)
+	{
+		hexadecimalNum[i] = n % 16;
+		n /= 16;
+		i++;
+	}
+
+	//printing hexadecimal number array in reverse order
+	for (int j = i - 1; j >= 0; j--)
+	{
+		cout << hexadecimalNum[j];
+	}
+}
 
 int main()
 {
-	int n = 1234;
+	int oct = 1234;
+	int hex = 1234;
 
-	decToOct(n);
+	decToOct(oct);
+	decToHex(hex);
 
     keep_window_open();
     return 0;
