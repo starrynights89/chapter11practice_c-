36ab3fbbc29b0aa25222e450dd658d3481ff43a7
Alexander Hartson <alexander-hartson@live.com>
2018-01-03 21:17:16 -0600

    Update Test_output.cpp

--- chapter11practice/Test_output.cpp
+++ chapter11practice/Test_output.cpp
@@ -18,6 +18,8 @@ void AddressBook()
 {
 	book.push_back(Address("Nick","Maclaren",
 		"01223334761","nmm1@cam.ac.uk"));
+	book.push_back(Address("Alexander","Hartson",
+		"3197778743","alexander-hartson@live.com));
 	int a=0, b=0, c=0, d=0;
 	for(int i=0; i<int(book.size()); i++)
 	{
